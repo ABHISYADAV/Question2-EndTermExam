b27b68d5fda6e53fd166b05bb6ac51a7f027b6b9
ABHAY-ABHI <ABHAY-ABHI@ABHAY-ABHI-PC>
2019-06-22 20:49:55 +0530

    Test Cases Passed.

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -22,6 +22,8 @@ public static void main(String[] args) {
 public List<Integer> listWithDuplicates(List<Integer> number){
 	List <Integer> L1 = null;
 	//write your code here !! change the L1 to required value
+	
+	L1 = new ArrayList<Integer>(number);
 	return L1 ;
 }
 }
